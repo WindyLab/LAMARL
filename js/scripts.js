b4c263f530c0cd7dab15b1cd74e0b3f9b7f0ff9d
Guobin-Zhu <1723407685@qq.com>
2025-06-03 14:28:58 +0800

    Refactor code structure for improved readability and maintainability

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var url = 'images/framework.pdf';
+
+// Asynchronously download PDF
+pdfjsLib.getDocument(url).promise.then(function(pdf) {
+    // Fetch the first page
+    pdf.getPage(1).then(function(page) {
+        var scale = 1.5;
+        var viewport = page.getViewport({ scale: scale });
+
+        // Prepare canvas using PDF page dimensions
+        var canvas = document.getElementById('pdf-canvas');
+        var context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        // Render PDF page into canvas context
+        var renderContext = {
+            canvasContext: context,
+            viewport: viewport
+        };
+        page.render(renderContext);
+    });
+});
